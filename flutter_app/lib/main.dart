8452b1d03b3a359655186bb162e1aea7fedfd1d1
jerry <linhuihui225@gmail.com>
2018-12-16 19:48:39 +0800

    show saved list item

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -26,11 +26,38 @@ class RandomWordsState extends State<RandomWords> {
 
   final _saved = new Set<WordPair>();
 
+  void _pushSaved() {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return new ListTile(
+          title: new Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      });
+      final divided = ListTile.divideTiles(
+        context: context,
+        tiles: tiles,
+      ).toList();
+
+      return new Scaffold(
+        appBar: AppBar(
+          title: new Text("Saved Suggestions"),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
       body: _buildSuggestion(),
     );
